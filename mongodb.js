f5679885ca3bdf2fb59e48102c8094c21aff040d
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-26 02:27:17 -0400

    learned update and updatemany mongodb methods

--- mongodb.js
+++ mongodb.js
@@ -10,36 +10,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('tasks').findOne({ _id: new ObjectID("5d5f8d43ff208d7151f06dd6") }, (error, task) => {
-    if (error) { return console.log('Unable to fetch') }
-    console.log(task)
+  // db.collection('users').updateOne(
+  //   { _id: new ObjectID("5d5f8acafdc6896dd93b5d1b")
+  // }, {
+  //   $inc: {
+  //     age: 1
+  //   }
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
+  // })
+
+  db.collection('tasks').updateMany({
+    completed: false
+  },{
+      $set: {
+        completed: true
+      }
+    }
+  ).then((result) => {
+    console.log(result.modifiedCount)
+  }).catch((error) => {
+    console.log(error)
   })
-
-  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    if (error) { return console.log('Unable to fetch') }
-    console.log(tasks)
-  })
-
-  // db.collection('users').findOne({ _id: new ObjectID("5d63304560022ad1af907bd4") }, (error, user) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-  //   console.log(user)
-  // })
-
-  // db.collection('users').find({ name: "Doug" }).toArray((error, users) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-  //   console.log(users)
-  // })
-
-  // db.collection('users').find({ name: "Doug" }).count((error, count) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-  //   console.log(count)
-  // })
-  
-
 })
